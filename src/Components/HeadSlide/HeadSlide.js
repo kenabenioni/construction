f69765ad2c002079fd49584f5bce5d568cdc7920
kenabenioni <kenabenioni@gmail.com>
2018-12-19 22:50:03 -1000

    changes

---
+++ src/Components/HeadSlide/HeadSlide.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class HeadSlide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div className="HeadSlide">
+
+      </div>
+    );
+  }
+}
+ 
+export default HeadSlide;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Nav.css';
+import {Link} from 'react-router-dom';
+
+class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div className="navwrapper">
+            <div className="Nav">
+              <div className="titlenav">
+              <Link to={"/"}>
+                <h2 id="titletext">CONSTRUCTION CO.</h2>
+              </Link>
+              </div>
+              <div className="rightnav">
+              <Link to={"/"} className="change">
+                <h3 id="navlinks">HOME</h3>
+              </Link>
+                <h3 id="navlinks">GALLERY</h3>
+                <h3 id="navlinks">BUY</h3>
+    
+              </div>
+            </div>
+            <hr />
+          </div>
+        );
+    }
+}
+ 
+export default Nav;
